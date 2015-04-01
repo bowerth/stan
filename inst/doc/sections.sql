cfe25cb73fa139ddb662c2b7d297f44b7317a2db
Bo Werth <bo.werth@gmail.com>
2015-04-01 21:47:06 +0200

    commit manual file deletion

--- inst/doc/sections.sql
+++
@@ -1,5 +0,0 @@
-drop table sections;
-create table sections(sec smallint, label varchar(50));
-insert into sections values(1, 'label section 1');
-insert into sections values(2, 'label section 2');
-select * from sections;
